a3a20f9b5a055658f11ab6ff429d8320d192b92d
Thalisu <thalisondmcc@gmail.com>
2025-01-11 02:32:06 -0300

    style: update paddings

--- src/components/homePage/HeroSection.vue
+++ src/components/homePage/HeroSection.vue
@@ -27,16 +27,16 @@ onMounted(() => {
 })
 </script>
 <template>
-  <div class="bg-tertiary w-svw flex justify-center px-4">
+  <div class="bg-tertiary w-svw flex justify-center px-4 overflow-hidden">
     <div
-      class="w-full max-w-max flex gap-4 max-h-screen overflow-clip items-start"
+      class="w-full max-w-max flex gap-4 overflow-clip items-start max-h-[70svh]"
       :style="containerStyle"
     >
       <div class="flex flex-col pt-10 gap-8 h-fit mr-auto" :style="textContainerStyle">
         <h2 class="font-primary text-6xl font-bold">Encontre roupas que fazem seu estilo</h2>
         <p class="text-secondary font-normal">
           Uma quantidade diversa de roupas de altissima qualidade, feitas para trazer a sua
-          individualidade e atender o seu senso de estilo
+          individualidade e atender o seu estilo
         </p>
         <button
           class="font-medium text-tertiary bg-complement py-4 px-12 rounded-full"
@@ -45,15 +45,15 @@ onMounted(() => {
           Compre Agora
         </button>
         <div class="flex pt-4 pb-10 mr-auto ml-auto">
-          <div class="flex flex-col gap-2 pr-8">
+          <div class="flex flex-col gap-2 pr-5">
             <span class="font-bold text-4xl">200+</span>
             <p class="font-normal text-secondary">Marcas Internacionais</p>
           </div>
-          <div class="flex flex-col gap-2 px-8 border-x-2">
+          <div class="flex flex-col gap-2 px-5 border-x-2">
             <span class="font-bold text-4xl">2.000+</span>
             <p class="font-normal text-secondary">Produtos de Qualidade</p>
           </div>
-          <div class="flex flex-col gap-2 pl-8">
+          <div class="flex flex-col gap-2 pl-5">
             <span class="font-bold text-4xl">30.000+</span>
             <p class="font-normal text-secondary">Clientes Satisfeitos</p>
           </div>
