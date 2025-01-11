76c4a766852a605b366c9f3c3a03605ee60e2907
Thalisu <thalisondmcc@gmail.com>
2025-01-11 17:35:58 -0300

    feat: add mocked products

---
+++ src/mock/products.ts
@@ -0,0 +1,55 @@
+const products = [
+  {
+    id: 0,
+    title: 'Camisa listrada vertical',
+    price: 232,
+    images: ['/src/assets/imgs/productMock/shirt1.png'],
+    discont: 0.2,
+  },
+  {
+    id: 1,
+    title: 'Calça jeans fina',
+    price: 260,
+    images: ['/src/assets/imgs/productMock/jeans3.png'],
+    discont: 0.2,
+  },
+  {
+    id: 2,
+    title: 'Camisa xadrex',
+    price: 180,
+    images: ['/src/assets/imgs/productMock/shirt3.png'],
+  },
+  {
+    id: 3,
+    title: 'Camisa listrada de manga',
+    price: 160,
+    images: ['/src/assets/imgs/productMock/shirt2.png'],
+    discont: 0.3,
+  },
+  {
+    id: 4,
+    title: 'Camisa com manga detalhada',
+    price: 120,
+    images: ['/src/assets/imgs/productMock/shirt5.png'],
+  },
+  {
+    id: 5,
+    title: 'Camisa courage',
+    price: 145,
+    images: ['/src/assets/imgs/productMock/shirt4.png'],
+  },
+  {
+    id: 6,
+    title: 'bermuda folgada jeans',
+    price: 80,
+    images: ['/src/assets/imgs/productMock/jeans2.png'],
+  },
+  {
+    id: 7,
+    title: 'Jeans faded',
+    price: 210,
+    images: ['/src/assets/imgs/productMock/jeans1.png'],
+  },
+]
+
+export default products
