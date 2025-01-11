afbf8468645198f9715ecb06bfd6d5f50ac7ec57
Thalisu <thalisondmcc@gmail.com>
2025-01-10 21:34:21 -0300

    chore: update imports

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,9 @@ export default {
       maxWidth: {
         max: 'var(--max-width)',
       },
+      gradientColorStops: {
+        complement: 'var(--background-complement)',
+      },
     },
   },
   plugins: ['prettier-plugin-tailwindcss'],
