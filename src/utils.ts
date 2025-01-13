e447f0ec8f79640d68496aad3c52a3bc16e09a69
Thalisu <thalisondmcc@gmail.com>
2025-01-13 20:39:24 -0300

    feat: add utility functions for gap retrieval and conditional rounding

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,13 @@
 export const isWidthLessThan = (x: number) => window.innerWidth < x
+export const getGap = (container: HTMLElement) =>
+  Number(getComputedStyle(container).gap.replace('px', ''))
+
+export const conditionalRound = (number: number, precision: number): number => {
+  const nextInt = Math.ceil(number)
+  console.log(nextInt - number)
+  if (nextInt - number <= precision / 100) {
+    return nextInt
+  }
+  const int = Math.floor(number)
+  return int
+}
