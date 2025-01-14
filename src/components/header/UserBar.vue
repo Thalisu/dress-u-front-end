16af6592b9385f71886aac6de6387908f25d543c
Thalisu <thalisondmcc@gmail.com>
2025-01-13 21:21:52 -0300

    refactor: remove mobile detection logic from SearchBar and UserBar components

--- src/components/header/UserBar.vue
+++ src/components/header/UserBar.vue
@@ -1,27 +1,11 @@
 <script lang="ts" setup>
 import CartSVG from '../svgs/CartSVG.vue'
 import UserSVG from '../svgs/UserSVG.vue'
-import { isWidthLessThan } from '@/utils'
-import { onMounted, ref } from 'vue'
 import SearchSVG from '../svgs/SearchSVG.vue'
-
-const isMobile = ref(false)
-
-const updateStyle = () => {
-  if (isWidthLessThan(400)) {
-    isMobile.value = true
-  } else {
-    isMobile.value = false
-  }
-}
-
-onMounted(() => {
-  window.addEventListener('resize', updateStyle)
-})
 </script>
 <template>
   <div class="flex gap-4">
-    <div v-if="isMobile" class="relative w-6 h-6">
+    <div class="relative w-6 h-6 md:hidden">
       <button>
         <SearchSVG class="w-6 h-6" />
       </button>
